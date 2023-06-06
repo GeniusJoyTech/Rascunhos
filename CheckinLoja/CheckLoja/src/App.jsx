081b5077eae5dc30e1f0630511d894e9c277f133
GeniusJoyTech <geniusjoytech@gmail.com>
2023-06-06 19:57:09 -0300

    Criando um novo rascunho de projeto

--- CheckinLoja/CheckLoja/src/App.jsx
+++ CheckinLoja/CheckLoja/src/App.jsx
@@ -1,8 +1,16 @@
-
+import './Components/svg/ArrowLeft'
+import Menu from './Components/menu/Menu'
+import ArrowLeft from './Components/svg/ArrowLeft'
+import ArrowRight from './Components/svg/ArrowRight'
 function App() {
 
   return (
     <>
+    <h1>Isso é uma bancada para Criação de componentes.</h1>
+    <Menu>
+      <ArrowLeft />
+      <ArrowRight />
+    </Menu>
       
     </>
   )
---
+++ CheckinLoja/CheckLoja/src/Components/menu/Menu.jsx
@@ -0,0 +1,8 @@
+const Menu = ({ children }) => {
+    return (
+      <div className="menu">
+        {children}
+      </div>
+    );
+  };
+export default Menu;
