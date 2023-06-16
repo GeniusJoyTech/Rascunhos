8baed1ea8ba7848910582761856f0ab760fac361
GeniusJoyTech <geniusjoytech@gmail.com>
2023-06-16 17:46:56 -0300

    Atualização no tabuleiro, agora todas as peças estão em suas casas iniciais

--- Xadrez/public/pages/Tabuleiro.jsx
+++ Xadrez/public/pages/Tabuleiro.jsx
@@ -6,6 +6,16 @@ import PeaoB from '../Components/Peças/PeaoB'
 import PeaoP from '../Components/Peças/PeaoP'
 
 import './Tabuleiro.css'
+import BispoP from '../Components/Peças/BispoP'
+import BispoB from '../Components/Peças/BispoB'
+import TorreB from '../Components/Peças/TorreB'
+import TorreP from '../Components/Peças/TorreP'
+import CavaloP from '../Components/Peças/CavaloP'
+import CavaloB from '../Components/Peças/CavaloB'
+import ReiB from '../Components/Peças/ReiB'
+import ReiP from '../Components/Peças/ReiP'
+import RainhaB from '../Components/Peças/RainhaB'
+import RainhaP from '../Components/Peças/RainhaP'
 function Tabuleiro() {
 
   return (
@@ -13,86 +23,86 @@ function Tabuleiro() {
       <main>
         <div className='flexivel'>
           <div id="a" >
-            <CasaBranca className="_8" />
-            <CasaPreta className="_7" peca={<PeaoP/>}/>
-            <CasaBranca className="_6"/>
-            <CasaPreta className="_5"/>
-            <CasaBranca className="_4"/>
-            <CasaPreta className="_3"/>
-            <CasaBranca className="_2" peca={<PeaoB/>}/>
-            <CasaPreta className="_1"/>
+            <CasaBranca peca={<TorreP id="TorreP"/>} />
+            <CasaPreta peca={<PeaoP />} />
+            <CasaBranca  />
+            <CasaPreta />
+            <CasaBranca  />
+            <CasaPreta  />
+            <CasaBranca  peca={<PeaoB />} />
+            <CasaPreta  peca={<TorreB />} />
           </div>
           <div id="b">
-            <CasaPreta  className="_8"/>
-            <CasaBranca className="_7" peca={<PeaoP/>}/>
-            <CasaPreta  className="_6"/>
-            <CasaBranca className="_5"/>
-            <CasaPreta  className="_4"/>
-            <CasaBranca className="_3"/>
-            <CasaPreta  className="_2" peca={<PeaoB/>}/>
-            <CasaBranca className="_1"/>
+            <CasaPreta peca={<CavaloP />} />
+            <CasaBranca peca={<PeaoP />} />
+            <CasaPreta  />
+            <CasaBranca />
+            <CasaPreta />
+            <CasaBranca />
+            <CasaPreta peca={<PeaoB />} />
+            <CasaBranca peca={<CavaloB />} />
 
           </div>
           <div id="c">
-            <CasaBranca className="_8"/>
-            <CasaPreta  className="_7" peca={<PeaoP/>}/>
-            <CasaBranca className="_6"/>
-            <CasaPreta  className="_5"/>
-            <CasaBranca className="_4"/>
-            <CasaPreta  className="_3"/>
-            <CasaBranca className="_2" peca={<PeaoB/>}/>
-            <CasaPreta  className="_1"/>
+            <CasaBranca  peca={<BispoP />} />
+            <CasaPreta  peca={<PeaoP />} />
+            <CasaBranca  />
+            <CasaPreta  />
+            <CasaBranca  />
+            <CasaPreta  />
+            <CasaBranca  peca={<PeaoB />} />
+            <CasaPreta  peca={<BispoB />} />
           </div>
           <div id="d">
-            <CasaPreta  className="_8"/>
-            <CasaBranca className="_7" peca={<PeaoP/>}/>
-            <CasaPreta  className="_6"/>
-            <CasaBranca className="_5"/>
-            <CasaPreta  className="_4"/>
-            <CasaBranca className="_3"/>
-            <CasaPreta  className="_2" peca={<PeaoB/>}/>
-            <CasaBranca className="_1"/>
+            <CasaPreta  peca={<RainhaP />}/>
+            <CasaBranca  peca={<PeaoP />} />
+            <CasaPreta  />
+            <CasaBranca />
+            <CasaPreta  />
+            <CasaBranca  />
+            <CasaPreta  peca={<PeaoB />} />
+            <CasaBranca  peca={<RainhaB />}/>
 
           </div>
           <div id="e">
-            <CasaBranca className="_8"/>
-            <CasaPreta  className="_7" peca={<PeaoP/>}/>
-            <CasaBranca className="_6"/>
-            <CasaPreta  className="_5"/>
-            <CasaBranca className="_4"/>
-            <CasaPreta  className="_3"/>
-            <CasaBranca className="_2" peca={<PeaoB/>}/>
-            <CasaPreta  className="_1"/>
+            <CasaBranca  peca={ <ReiP/> } />
+            <CasaPreta  peca={<PeaoP />} />
+            <CasaBranca  />
+            <CasaPreta  />
+            <CasaBranca  />
+            <CasaPreta  />
+            <CasaBranca  peca={<PeaoB />} />
+            <CasaPreta   peca={ <ReiB/> }/>
           </div>
           <div id="f">
-            <CasaPreta  className="_8"/>
-            <CasaBranca className="_7" peca={<PeaoP/>}/>
-            <CasaPreta  className="_6"/>
-            <CasaBranca className="_5"/>
-            <CasaPreta  className="_4"/>
-            <CasaBranca className="_3"/>
-            <CasaPreta  className="_2" peca={<PeaoB/>}/>
-            <CasaBranca className="_1"/>
+            <CasaPreta  peca={<BispoP />} />
+            <CasaBranca  peca={<PeaoP />} />
+            <CasaPreta  />
+            <CasaBranca />
+            <CasaPreta  />
+            <CasaBranca  />
+            <CasaPreta  peca={<PeaoB />} />
+            <CasaBranca  peca={<BispoB />} />
           </div>
           <div id="g">
-            <CasaBranca className="_8"/>
-            <CasaPreta  className="_7" peca={<PeaoP/>}/>
-            <CasaBranca className="_6"/>
-            <CasaPreta  className="_5"/>
-            <CasaBranca className="_4"/>
-            <CasaPreta  className="_3"/>
-            <CasaBranca className="_2" peca={<PeaoB/>}/>
-            <CasaPreta  className="_1"/>
+            <CasaBranca  peca={<CavaloP />} />
+            <CasaPreta  peca={<PeaoP />} />
+            <CasaBranca  />
+            <CasaPreta  />
+            <CasaBranca  />
+            <CasaPreta  />
+            <CasaBranca  peca={<PeaoB />} />
+            <CasaPreta  peca={<CavaloB />} />
           </div>
           <div id="h">
-            <CasaPreta  className="_8"/>
-            <CasaBranca className="_7" peca={<PeaoP/>}/>
-            <CasaPreta  className="_6"/>
-            <CasaBranca className="_5"/>
-            <CasaPreta  className="_4"/>
-            <CasaBranca className="_3"/>
-            <CasaPreta  className="_2" peca={<PeaoB/>}/>
-            <CasaBranca className="_1"/>
+            <CasaPreta  peca={<TorreP />} />
+            <CasaBranca  peca={<PeaoP />} />
+            <CasaPreta  />
+            <CasaBranca />
+            <CasaPreta  />
+            <CasaBranca  />
+            <CasaPreta  peca={<PeaoB />} />
+            <CasaBranca  peca={<TorreB />} />
           </div>
         </div>
       </main>
