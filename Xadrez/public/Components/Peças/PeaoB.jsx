abb4b59e478ca78d790e9629fd8da5b43e55cb94
GeniusJoyTech <geniusjoytech@gmail.com>
2023-06-15 22:36:33 -0300

    Atualização do jogo de xadrez, tabuleiro e peça de peao

---
+++ Xadrez/public/Components/Peças/PeaoB.jsx
@@ -0,0 +1,11 @@
+import './css/pecas.css'
+const PeaoB = () => {
+    return (
+        <svg className='peao _b' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <circle cx="12" cy="16" r="4" fill="#000" />
+            <path d="M12 2C9.24 2 7 4.24 7 7c0 1.95 1.1 3.63 2.71 4.5v6.79c0 .41.34.75.75.75h2.5c.41 0 .75-.34.75-.75V11.5C15.9 10.63 17 8.95 17 7c0-2.76-2.24-5-5-5zm0 9c-1.66 0-3-1.34-3-3 0-1.66 1.34-3 3-3s3 1.34 3 3c0 1.66-1.34 3-3 3z" fill="#000" />
+        </svg>
+
+    );
+} 
+export default PeaoB;
--- Xadrez/public/pages/Tabuleiro.jsx
+++ Xadrez/public/pages/Tabuleiro.jsx
@@ -2,30 +2,101 @@ import { useState } from 'react'
 import CasaPreta from '../Components/Casas/CasaPreta'
 import CasaBranca from '../Components/Casas/CasaBranca'
 
+import PeaoB from '../Components/Peças/PeaoB'
+
+import './Tabuleiro.css'
 function Tabuleiro() {
 
   return (
     <>
-      
-    <div id="a" className='flexivel'>
-      <CasaBranca id="_8"/>
-      <CasaPreta id="_7"/>
-      <CasaBranca id="_6"/>
-      <CasaPreta id="_5"/>
-      <CasaBranca id="_4"/>
-      <CasaPreta id="_3"/>
-      <CasaBranca id="_2"/>
-      <CasaPreta id="_1"/>
-    
-    </div>
-    <div id="b" className='flexivel'></div>
-    <div id="c" className='flexivel'></div>
-    <div id="d" className='flexivel'></div>
-    <div id="e" className='flexivel'></div>
-    <div id="f" className='flexivel'></div>
-    <div id="g" className='flexivel'></div>
+      <main>
+        <div className='flexivel'>
+          <div id="a" >
+            <CasaBranca className="_8" peca={<PeaoB/>}/>
+            <CasaPreta className="_7" />
+            <CasaBranca className="_6"/>
+            <CasaPreta className="_5"/>
+            <CasaBranca className="_4"/>
+            <CasaPreta className="_3"/>
+            <CasaBranca className="_2" />
+            <CasaPreta className="_1"/>
+          </div>
+          <div id="b">
+            <CasaPreta className="_1"></CasaPreta>
+            <CasaBranca className="_2"></CasaBranca>
+            <CasaPreta className="_3"></CasaPreta>
+            <CasaBranca className="_4"></CasaBranca>
+            <CasaPreta className="_5"></CasaPreta>
+            <CasaBranca className="_6"></CasaBranca>
+            <CasaPreta className="_7"></CasaPreta>
+            <CasaBranca className="_8"></CasaBranca>
+
+          </div>
+          <div id="c">
+            <CasaBranca className="_1"></CasaBranca>
+            <CasaPreta className="_2"></CasaPreta>
+            <CasaBranca className="_3"></CasaBranca>
+            <CasaPreta className="_4"></CasaPreta>
+            <CasaBranca className="_5"></CasaBranca>
+            <CasaPreta className="_6"></CasaPreta>
+            <CasaBranca className="_7"></CasaBranca>
+            <CasaPreta className="_8"></CasaPreta>
+          </div>
+          <div id="d">
+            <CasaPreta className="_1"></CasaPreta>
+            <CasaBranca className="_2"></CasaBranca>
+            <CasaPreta className="_3"></CasaPreta>
+            <CasaBranca className="_4"></CasaBranca>
+            <CasaPreta className="_5"></CasaPreta>
+            <CasaBranca className="_6"></CasaBranca>
+            <CasaPreta className="_7"></CasaPreta>
+            <CasaBranca className="_8"></CasaBranca>
+
+          </div>
+          <div id="e">
+            <CasaBranca className="_1"></CasaBranca>
+            <CasaPreta className="_2"></CasaPreta>
+            <CasaBranca className="_3"></CasaBranca>
+            <CasaPreta className="_4"></CasaPreta>
+            <CasaBranca className="_5"></CasaBranca>
+            <CasaPreta className="_6"></CasaPreta>
+            <CasaBranca className="_7"></CasaBranca>
+            <CasaPreta className="_8"></CasaPreta>
+          </div>
+          <div id="f">
+            <CasaPreta className="_1"></CasaPreta>
+            <CasaBranca className="_2"></CasaBranca>
+            <CasaPreta className="_3"></CasaPreta>
+            <CasaBranca className="_4"></CasaBranca>
+            <CasaPreta className="_5"></CasaPreta>
+            <CasaBranca className="_6"></CasaBranca>
+            <CasaPreta className="_7"></CasaPreta>
+            <CasaBranca className="_8"></CasaBranca>
+          </div>
+          <div id="g">
+            <CasaBranca className="_1"></CasaBranca>
+            <CasaPreta className="_2"></CasaPreta>
+            <CasaBranca className="_3"></CasaBranca>
+            <CasaPreta className="_4"></CasaPreta>
+            <CasaBranca className="_5"></CasaBranca>
+            <CasaPreta className="_6"></CasaPreta>
+            <CasaBranca className="_7"></CasaBranca>
+            <CasaPreta className="_8"></CasaPreta>
+          </div>
+          <div id="h">
+            <CasaPreta className="_1"></CasaPreta>
+            <CasaBranca className="_2"></CasaBranca>
+            <CasaPreta className="_3"></CasaPreta>
+            <CasaBranca className="_4"></CasaBranca>
+            <CasaPreta className="_5"></CasaPreta>
+            <CasaBranca className="_6"></CasaBranca>
+            <CasaPreta className="_7"></CasaPreta>
+            <CasaBranca className="_8"></CasaBranca>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
 export default Tabuleiro
