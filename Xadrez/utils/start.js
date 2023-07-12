926931c89d802630bcfec6b250f63080bf0d333f
GeniusJoyTech <geniusjoytech@gmail.com>
2023-07-12 11:54:34 -0300

    Novo commit.

--- Xadrez/utils/start.js
+++ Xadrez/utils/start.js
@@ -1,4 +1,38 @@
 //Funcao para iniciar ou reiniciar o jogo.
 //Sera chamada quando o usuario entrar na pagina ou clicar em atualizar.
 
- 
+ /*
+
+    chamar as seguintes funções
+        limparTabuleiro(){
+            for(let i = 97; i < 105; i++){
+                let coluna =  String.fromCharCode(i);
+                for(let linha = 1; linha < 9; linha++){
+                    endCasa = coluna + linha;
+                    var casa = document.getElementById(endCasa);
+                    casa.   
+                }
+        }
+        
+        peoes(2){};
+        peoes(7);
+
+        torre();
+        cavalo();
+        bispo();
+        rei();
+        rainha();
+
+        
+
+
+
+    Primeiro deve remover as peças do tabuleiro.<-Pode ser uma função separada
+        Basta apenas jogar no valor da peca o valor '' logo será peca=''
+    Após isso devemos adicionar peça por peça no seu respectivo local
+    peãos podem ser adicionados por meio de um loop for, por exemplo
+        for(let i = 97; i < 105; i++){
+            casa =  String.fromCharCode(asciiValue) + 8;
+
+        }
+ */
